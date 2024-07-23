f902d5c13ba1234c3e04cda67dcee0abfbaf4da7
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-07-23 16:27:44 -0300

    feat: :sparkles: add calendar event and helpers to funtionality

---
+++ src/calendar/components/CalendarEvent.jsx
@@ -0,0 +1,10 @@
+export const CalendarEvent = ({ event }) => {
+  const { title, user } = event;
+  //   console.log(event);
+  return (
+    <>
+      <strong>{title}</strong>
+      <span> - {user.name}</span>
+    </>
+  );
+};
