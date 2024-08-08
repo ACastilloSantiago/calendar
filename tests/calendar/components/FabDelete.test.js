b01b6d14fb9e80d25c2610a06269c40e68817845
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-08-08 16:00:33 -0300

    feat: :white_check_mark: add test to fabDelete

--- tests/calendar/components/FabDelete.test.js
+++ tests/calendar/components/FabDelete.test.js
@@ -1,16 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { FabDelete } from '../../../src/calendar/components/FabDelete';
-import { Provider } from 'react-redux';
-import { store } from '../../../src/store';
+import { useCalendarStore } from '../../../src/hooks';
+// * Mock del hook
+jest.mock('../../../src/hooks/useCalendarStore.js');
 
 describe('Pruebas en el componente <FabDelete />', () => {
   test('Debe de mostrar el componente correctamente', () => {
-    render(
-      <Provider store={store}>
-        <FabDelete />
-      </Provider>
-    );
+    // * Indicamos el restorno al utilizar el mock ( hook ).
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: false,
+    });
 
-    screen.debug();
+    render(<FabDelete />);
+
+    // * Tomamos la referencia del boton
+    const btn = screen.getByLabelText('btn-delete');
+
+    expect(btn.classList).toContain('btn');
+    expect(btn.classList).toContain('btn-danger');
+    expect(btn.classList).toContain('fab-danger');
+    expect(btn.style.display).toBe('none');
+  });
+  test('Debe de mostrar el boton si hay un evento activo', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText('btn-delete');
+
+    expect(btn.style.display).toBe('');
+  });
+
+  const mockStartDeletingEvent = jest.fn();
+
+  beforeEach(() => jest.clearAllMocks());
+
+  test('Deve de llamar startDeletingEvent', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+      startDeletingEvent: mockStartDeletingEvent,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText('btn-delete');
+
+    fireEvent.click(btn);
+
+    expect(mockStartDeletingEvent).toHaveBeenCalled();
   });
 });
